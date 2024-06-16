b2e348caab142fc289f99363a9c998f9bbc15fe5
Cao Yang <yang.cao@hytechc.com>
2024-06-16 14:04:40 +0800

    add throttle compare

---
+++ src/Benchmark/Benchmark.Throttle/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.RateLimiting;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmark.Throttle;
+
+[SimpleJob(RuntimeMoniker.Net80)]
+[MemoryDiagnoser]
+[RPlotExporter]
+public class ThrottleBenchmark
+{
+    [Params(10, 100, 1000, 10000)]
+    public int LimitPerSecond;
+
+    private Throttle _throttle;
+    private FixedWindowRateLimiter _fixedLimiter;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _throttle = Benchmark.Throttle.Throttle.Create(LimitPerSecond);
+        _fixedLimiter = new FixedWindowRateLimiter(new FixedWindowRateLimiterOptions
+        {
+            Window = TimeSpan.FromSeconds(1),
+            AutoReplenishment = true,
+            PermitLimit = LimitPerSecond,
+            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+            QueueLimit = 0
+        });
+    }
+
+    [Benchmark]
+    public void Throttle()
+    {
+        _throttle.TryAcquire();
+    }
+
+    [Benchmark]
+    public async Task RateLimiter()
+    {
+        var lease = await _fixedLimiter.AcquireAsync();
+        if (lease.IsAcquired)
+        {
+        }
+    }
+}
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkDotNet.Running.BenchmarkRunner.Run<ThrottleBenchmark>();
+    }
+
+    public static void Print(IList<DateTimeOffset> timestamps)
+    {
+        foreach (var group in timestamps.GroupBy(x => new
+        {
+            x.Year,
+            x.Month,
+            x.Day,
+            x.Hour,
+            x.Minute,
+            x.Second
+        }))
+        {
+            Console.WriteLine($"{group.Key} {group.Count()}");
+        }
+    }
+}
---
+++ src/Benchmark/Benchmark.Throttle/Throttle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Benchmark.Throttle;
+
+public interface IDateTimeProvider
+{
+    DateTime UtcNow { get; }
+}
+
+public class SystemDateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
+
+public sealed class Throttle(TimeSpan window, int limit, IDateTimeProvider dateTimeProvider)
+    : IDisposable
+{
+    private readonly TimeSpan _window = TimeSpan.FromMilliseconds(window.TotalMilliseconds / limit);
+    private readonly ConcurrentDictionary<string, (object, Queue<DateTime>)> _lockObjects = new();
+    private const string DEFAULT_THROTTLE_KEY = "";
+
+    public static Throttle Create(int limit)
+    {
+        return Create(TimeSpan.FromSeconds(1), limit);
+    }
+
+    public static Throttle Create(TimeSpan window, int limit)
+    {
+        return new Throttle(window, limit, new SystemDateTimeProvider());
+    }
+
+    public bool TryAcquire(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            throw new ArgumentException("key can not be null or empty", nameof(key));
+        }
+
+        return TryAcquireInternal(key);
+    }
+
+    public bool TryAcquire()
+    {
+        return TryAcquireInternal(DEFAULT_THROTTLE_KEY);
+    }
+
+    private bool TryAcquireInternal(string key)
+    {
+        var now = dateTimeProvider.UtcNow;
+        (object lockObject, Queue<DateTime> timeStamps) = _lockObjects.GetOrAdd(key, _ => (new object(), new Queue<DateTime>()));
+
+        lock (lockObject)
+        {
+            // Remove expired timestamps
+            while (timeStamps.Count > 0 && timeStamps.Peek() <= now - _window)
+            {
+                timeStamps.Dequeue();
+            }
+
+            // Check rate limit
+            if (timeStamps.Count > 0)
+            {
+                return false;
+            }
+
+            // Add new timestamp
+            timeStamps.Enqueue(now);
+        }
+
+        return true;
+    }
+
+    public void Dispose()
+    {
+        _lockObjects.Clear();
+    }
+}
